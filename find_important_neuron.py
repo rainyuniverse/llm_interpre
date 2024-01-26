0dda4dc3f79c0d0e63c14407572f04dab348294c
rainyuniverse <1075635501@qq.com>
2024-01-27 00:27:39 +0800

    find important neuron & cluster visualize

--- find_important_neuron.py
+++ find_important_neuron.py
@@ -67,32 +67,34 @@ if __name__ == "__main__":
     folder_path = "/data/lypan/llm_interpre/neuron_info/test/"
 
     target_module_name_list = find_all_target_modules(bloom)
+    # 目标结构名称列表
+    target_module_names = target_module_name_list[2:-2]
 
-    target_module_names = ['transformer.h.0.input_layernorm', 'transformer.h.0.self_attention.query_key_value', 'transformer.h.0.self_attention.dense', 'transformer.h.0.post_attention_layernorm', 'transformer.h.0.mlp.dense_h_to_4h', 'transformer.h.0.mlp.dense_4h_to_h', 
-                       'transformer.h.1.input_layernorm', 'transformer.h.1.self_attention.query_key_value', 'transformer.h.1.self_attention.dense', 'transformer.h.1.post_attention_layernorm', 'transformer.h.1.mlp.dense_h_to_4h', 'transformer.h.1.mlp.dense_4h_to_h', 
-                       'transformer.h.2.input_layernorm', 'transformer.h.2.self_attention.query_key_value', 'transformer.h.2.self_attention.dense', 'transformer.h.2.post_attention_layernorm', 'transformer.h.2.mlp.dense_h_to_4h', 'transformer.h.2.mlp.dense_4h_to_h', 
-                       'transformer.h.3.input_layernorm', 'transformer.h.3.self_attention.query_key_value', 'transformer.h.3.self_attention.dense', 'transformer.h.3.post_attention_layernorm', 'transformer.h.3.mlp.dense_h_to_4h', 'transformer.h.3.mlp.dense_4h_to_h', 
-                       'transformer.h.4.input_layernorm', 'transformer.h.4.self_attention.query_key_value', 'transformer.h.4.self_attention.dense', 'transformer.h.4.post_attention_layernorm', 'transformer.h.4.mlp.dense_h_to_4h', 'transformer.h.4.mlp.dense_4h_to_h', 
-                       'transformer.h.5.input_layernorm', 'transformer.h.5.self_attention.query_key_value', 'transformer.h.5.self_attention.dense', 'transformer.h.5.post_attention_layernorm', 'transformer.h.5.mlp.dense_h_to_4h', 'transformer.h.5.mlp.dense_4h_to_h', 
-                       'transformer.h.6.input_layernorm', 'transformer.h.6.self_attention.query_key_value', 'transformer.h.6.self_attention.dense', 'transformer.h.6.post_attention_layernorm', 'transformer.h.6.mlp.dense_h_to_4h', 'transformer.h.6.mlp.dense_4h_to_h', 
-                       'transformer.h.7.input_layernorm', 'transformer.h.7.self_attention.query_key_value', 'transformer.h.7.self_attention.dense', 'transformer.h.7.post_attention_layernorm', 'transformer.h.7.mlp.dense_h_to_4h', 'transformer.h.7.mlp.dense_4h_to_h', 
-                       'transformer.h.8.input_layernorm', 'transformer.h.8.self_attention.query_key_value', 'transformer.h.8.self_attention.dense', 'transformer.h.8.post_attention_layernorm', 'transformer.h.8.mlp.dense_h_to_4h', 'transformer.h.8.mlp.dense_4h_to_h', 
-                       'transformer.h.9.input_layernorm', 'transformer.h.9.self_attention.query_key_value', 'transformer.h.9.self_attention.dense', 'transformer.h.9.post_attention_layernorm', 'transformer.h.9.mlp.dense_h_to_4h', 'transformer.h.9.mlp.dense_4h_to_h', 
-                       'transformer.h.10.input_layernorm', 'transformer.h.10.self_attention.query_key_value', 'transformer.h.10.self_attention.dense', 'transformer.h.10.post_attention_layernorm', 'transformer.h.10.mlp.dense_h_to_4h', 'transformer.h.10.mlp.dense_4h_to_h', 
-                       'transformer.h.11.input_layernorm', 'transformer.h.11.self_attention.query_key_value', 'transformer.h.11.self_attention.dense', 'transformer.h.11.post_attention_layernorm', 'transformer.h.11.mlp.dense_h_to_4h', 'transformer.h.11.mlp.dense_4h_to_h', 
-                       'transformer.h.12.input_layernorm', 'transformer.h.12.self_attention.query_key_value', 'transformer.h.12.self_attention.dense', 'transformer.h.12.post_attention_layernorm', 'transformer.h.12.mlp.dense_h_to_4h', 'transformer.h.12.mlp.dense_4h_to_h', 
-                       'transformer.h.13.input_layernorm', 'transformer.h.13.self_attention.query_key_value', 'transformer.h.13.self_attention.dense', 'transformer.h.13.post_attention_layernorm', 'transformer.h.13.mlp.dense_h_to_4h', 'transformer.h.13.mlp.dense_4h_to_h', 
-                       'transformer.h.14.input_layernorm', 'transformer.h.14.self_attention.query_key_value', 'transformer.h.14.self_attention.dense', 'transformer.h.14.post_attention_layernorm', 'transformer.h.14.mlp.dense_h_to_4h', 'transformer.h.14.mlp.dense_4h_to_h', 
-                       'transformer.h.15.input_layernorm', 'transformer.h.15.self_attention.query_key_value', 'transformer.h.15.self_attention.dense', 'transformer.h.15.post_attention_layernorm', 'transformer.h.15.mlp.dense_h_to_4h', 'transformer.h.15.mlp.dense_4h_to_h', 
-                       'transformer.h.16.input_layernorm', 'transformer.h.16.self_attention.query_key_value', 'transformer.h.16.self_attention.dense', 'transformer.h.16.post_attention_layernorm', 'transformer.h.16.mlp.dense_h_to_4h', 'transformer.h.16.mlp.dense_4h_to_h', 
-                       'transformer.h.17.input_layernorm', 'transformer.h.17.self_attention.query_key_value', 'transformer.h.17.self_attention.dense', 'transformer.h.17.post_attention_layernorm', 'transformer.h.17.mlp.dense_h_to_4h', 'transformer.h.17.mlp.dense_4h_to_h', 
-                       'transformer.h.18.input_layernorm', 'transformer.h.18.self_attention.query_key_value', 'transformer.h.18.self_attention.dense', 'transformer.h.18.post_attention_layernorm', 'transformer.h.18.mlp.dense_h_to_4h', 'transformer.h.18.mlp.dense_4h_to_h', 
-                       'transformer.h.19.input_layernorm', 'transformer.h.19.self_attention.query_key_value', 'transformer.h.19.self_attention.dense', 'transformer.h.19.post_attention_layernorm', 'transformer.h.19.mlp.dense_h_to_4h', 'transformer.h.19.mlp.dense_4h_to_h', 
-                       'transformer.h.20.input_layernorm', 'transformer.h.20.self_attention.query_key_value', 'transformer.h.20.self_attention.dense', 'transformer.h.20.post_attention_layernorm', 'transformer.h.20.mlp.dense_h_to_4h', 'transformer.h.20.mlp.dense_4h_to_h', 
-                       'transformer.h.21.input_layernorm', 'transformer.h.21.self_attention.query_key_value', 'transformer.h.21.self_attention.dense', 'transformer.h.21.post_attention_layernorm', 'transformer.h.21.mlp.dense_h_to_4h', 'transformer.h.21.mlp.dense_4h_to_h', 
-                       'transformer.h.22.input_layernorm', 'transformer.h.22.self_attention.query_key_value', 'transformer.h.22.self_attention.dense', 'transformer.h.22.post_attention_layernorm', 'transformer.h.22.mlp.dense_h_to_4h', 'transformer.h.22.mlp.dense_4h_to_h', 
-                       'transformer.h.23.input_layernorm', 'transformer.h.23.self_attention.query_key_value', 'transformer.h.23.self_attention.dense', 'transformer.h.23.post_attention_layernorm', 'transformer.h.23.mlp.dense_h_to_4h', 'transformer.h.23.mlp.dense_4h_to_h', 
-    ]
+    # target_module_names = ['transformer.h.0.input_layernorm', 'transformer.h.0.self_attention.query_key_value', 'transformer.h.0.self_attention.dense', 'transformer.h.0.post_attention_layernorm', 'transformer.h.0.mlp.dense_h_to_4h', 'transformer.h.0.mlp.dense_4h_to_h', 
+    #                    'transformer.h.1.input_layernorm', 'transformer.h.1.self_attention.query_key_value', 'transformer.h.1.self_attention.dense', 'transformer.h.1.post_attention_layernorm', 'transformer.h.1.mlp.dense_h_to_4h', 'transformer.h.1.mlp.dense_4h_to_h', 
+    #                    'transformer.h.2.input_layernorm', 'transformer.h.2.self_attention.query_key_value', 'transformer.h.2.self_attention.dense', 'transformer.h.2.post_attention_layernorm', 'transformer.h.2.mlp.dense_h_to_4h', 'transformer.h.2.mlp.dense_4h_to_h', 
+    #                    'transformer.h.3.input_layernorm', 'transformer.h.3.self_attention.query_key_value', 'transformer.h.3.self_attention.dense', 'transformer.h.3.post_attention_layernorm', 'transformer.h.3.mlp.dense_h_to_4h', 'transformer.h.3.mlp.dense_4h_to_h', 
+    #                    'transformer.h.4.input_layernorm', 'transformer.h.4.self_attention.query_key_value', 'transformer.h.4.self_attention.dense', 'transformer.h.4.post_attention_layernorm', 'transformer.h.4.mlp.dense_h_to_4h', 'transformer.h.4.mlp.dense_4h_to_h', 
+    #                    'transformer.h.5.input_layernorm', 'transformer.h.5.self_attention.query_key_value', 'transformer.h.5.self_attention.dense', 'transformer.h.5.post_attention_layernorm', 'transformer.h.5.mlp.dense_h_to_4h', 'transformer.h.5.mlp.dense_4h_to_h', 
+    #                    'transformer.h.6.input_layernorm', 'transformer.h.6.self_attention.query_key_value', 'transformer.h.6.self_attention.dense', 'transformer.h.6.post_attention_layernorm', 'transformer.h.6.mlp.dense_h_to_4h', 'transformer.h.6.mlp.dense_4h_to_h', 
+    #                    'transformer.h.7.input_layernorm', 'transformer.h.7.self_attention.query_key_value', 'transformer.h.7.self_attention.dense', 'transformer.h.7.post_attention_layernorm', 'transformer.h.7.mlp.dense_h_to_4h', 'transformer.h.7.mlp.dense_4h_to_h', 
+    #                    'transformer.h.8.input_layernorm', 'transformer.h.8.self_attention.query_key_value', 'transformer.h.8.self_attention.dense', 'transformer.h.8.post_attention_layernorm', 'transformer.h.8.mlp.dense_h_to_4h', 'transformer.h.8.mlp.dense_4h_to_h', 
+    #                    'transformer.h.9.input_layernorm', 'transformer.h.9.self_attention.query_key_value', 'transformer.h.9.self_attention.dense', 'transformer.h.9.post_attention_layernorm', 'transformer.h.9.mlp.dense_h_to_4h', 'transformer.h.9.mlp.dense_4h_to_h', 
+    #                    'transformer.h.10.input_layernorm', 'transformer.h.10.self_attention.query_key_value', 'transformer.h.10.self_attention.dense', 'transformer.h.10.post_attention_layernorm', 'transformer.h.10.mlp.dense_h_to_4h', 'transformer.h.10.mlp.dense_4h_to_h', 
+    #                    'transformer.h.11.input_layernorm', 'transformer.h.11.self_attention.query_key_value', 'transformer.h.11.self_attention.dense', 'transformer.h.11.post_attention_layernorm', 'transformer.h.11.mlp.dense_h_to_4h', 'transformer.h.11.mlp.dense_4h_to_h', 
+    #                    'transformer.h.12.input_layernorm', 'transformer.h.12.self_attention.query_key_value', 'transformer.h.12.self_attention.dense', 'transformer.h.12.post_attention_layernorm', 'transformer.h.12.mlp.dense_h_to_4h', 'transformer.h.12.mlp.dense_4h_to_h', 
+    #                    'transformer.h.13.input_layernorm', 'transformer.h.13.self_attention.query_key_value', 'transformer.h.13.self_attention.dense', 'transformer.h.13.post_attention_layernorm', 'transformer.h.13.mlp.dense_h_to_4h', 'transformer.h.13.mlp.dense_4h_to_h', 
+    #                    'transformer.h.14.input_layernorm', 'transformer.h.14.self_attention.query_key_value', 'transformer.h.14.self_attention.dense', 'transformer.h.14.post_attention_layernorm', 'transformer.h.14.mlp.dense_h_to_4h', 'transformer.h.14.mlp.dense_4h_to_h', 
+    #                    'transformer.h.15.input_layernorm', 'transformer.h.15.self_attention.query_key_value', 'transformer.h.15.self_attention.dense', 'transformer.h.15.post_attention_layernorm', 'transformer.h.15.mlp.dense_h_to_4h', 'transformer.h.15.mlp.dense_4h_to_h', 
+    #                    'transformer.h.16.input_layernorm', 'transformer.h.16.self_attention.query_key_value', 'transformer.h.16.self_attention.dense', 'transformer.h.16.post_attention_layernorm', 'transformer.h.16.mlp.dense_h_to_4h', 'transformer.h.16.mlp.dense_4h_to_h', 
+    #                    'transformer.h.17.input_layernorm', 'transformer.h.17.self_attention.query_key_value', 'transformer.h.17.self_attention.dense', 'transformer.h.17.post_attention_layernorm', 'transformer.h.17.mlp.dense_h_to_4h', 'transformer.h.17.mlp.dense_4h_to_h', 
+    #                    'transformer.h.18.input_layernorm', 'transformer.h.18.self_attention.query_key_value', 'transformer.h.18.self_attention.dense', 'transformer.h.18.post_attention_layernorm', 'transformer.h.18.mlp.dense_h_to_4h', 'transformer.h.18.mlp.dense_4h_to_h', 
+    #                    'transformer.h.19.input_layernorm', 'transformer.h.19.self_attention.query_key_value', 'transformer.h.19.self_attention.dense', 'transformer.h.19.post_attention_layernorm', 'transformer.h.19.mlp.dense_h_to_4h', 'transformer.h.19.mlp.dense_4h_to_h', 
+    #                    'transformer.h.20.input_layernorm', 'transformer.h.20.self_attention.query_key_value', 'transformer.h.20.self_attention.dense', 'transformer.h.20.post_attention_layernorm', 'transformer.h.20.mlp.dense_h_to_4h', 'transformer.h.20.mlp.dense_4h_to_h', 
+    #                    'transformer.h.21.input_layernorm', 'transformer.h.21.self_attention.query_key_value', 'transformer.h.21.self_attention.dense', 'transformer.h.21.post_attention_layernorm', 'transformer.h.21.mlp.dense_h_to_4h', 'transformer.h.21.mlp.dense_4h_to_h', 
+    #                    'transformer.h.22.input_layernorm', 'transformer.h.22.self_attention.query_key_value', 'transformer.h.22.self_attention.dense', 'transformer.h.22.post_attention_layernorm', 'transformer.h.22.mlp.dense_h_to_4h', 'transformer.h.22.mlp.dense_4h_to_h', 
+    #                    'transformer.h.23.input_layernorm', 'transformer.h.23.self_attention.query_key_value', 'transformer.h.23.self_attention.dense', 'transformer.h.23.post_attention_layernorm', 'transformer.h.23.mlp.dense_h_to_4h', 'transformer.h.23.mlp.dense_4h_to_h', 
+    # ]
 
     forward_cache = []
     backward_cache = []
@@ -121,10 +123,16 @@ if __name__ == "__main__":
             for k in range(len(target_module_names)):
                 cur_module_name = target_module_names[k]
                 # 前向传播的表示和后向传播的梯度相乘得到重要性分数
+                # forward[k] [1, sent_len, hidden_states]
+                # backward_cache[len(backward_cache) - k - 1] [1, sent_len, hidden_states]
                 importance_matrix = (forward_cache[k] * backward_cache[len(backward_cache) - k - 1]).abs()
+                # 首先将batch_size维度去掉，形状变为[sent_len, hidden_states]
+                # 然后按照sent_len维度取平均值，形状变为[hidden_states]
                 importance_matrix = importance_matrix.view(-1, importance_matrix.size(-1)).mean(0)
+                # 将当前语言当前结构的重要性分数矩阵保存到字典中
                 importance_matrix_dict[cur_lang_code][cur_module_name].append(importance_matrix.detach().cpu())
 
+            # 保存的cache清零
             forward_cache = []
             backward_cache = []
             # 清零梯度
@@ -143,35 +151,40 @@ if __name__ == "__main__":
 
         for j in range(len(lang_code_list)):
             cur_lang_code = lang_code_list[j]
+            # 将当前语言当前结构所有句子的表示加和
             all_importance_matrix = torch.stack(importance_matrix_dict[cur_lang_code][cur_module_name]) # [sent_num, neuron_num]
             sum_importance_matrix = all_importance_matrix.sum(0)
             sum_importance_matrix_list.append(sum_importance_matrix)
 
-    all_sum_importance_matrix = torch.stack(sum_importance_matrix_list) #[lang_num, neuron_num]
-    all_sum_importance_matrix_var = torch.var(all_sum_importance_matrix, dim=0) #[neuron_num]
+        all_sum_importance_matrix = torch.stack(sum_importance_matrix_list) #[lang_num, neuron_num]
+        all_sum_importance_matrix_var = torch.var(all_sum_importance_matrix, dim=0) #[neuron_num]
 
-    # 按照方差大小对神经元进行排序
-    sorted_values, sorted_indices = torch.sort(all_sum_importance_matrix_var, descending=True)
+        # 按照方差大小对神经元进行排序
+        sorted_values, sorted_indices = torch.sort(all_sum_importance_matrix_var, descending=True)
 
-    ratio = 0.1
-    values, indices = torch.topk(all_sum_importance_matrix_var, (int)(ratio * all_sum_importance_matrix_var.size(0)))
+        # 语言特有神经元的比例
+        ratio = 0.1
+        values, indices = torch.topk(all_sum_importance_matrix_var, (int)(ratio * all_sum_importance_matrix_var.size(0)))
 
-    sorted_specific_indices, _ = torch.sort(indices)
-    all_indices = torch.arange(all_sum_importance_matrix_var.size(0))
-    mask = torch.isin(all_indices, sorted_specific_indices)
-    sorted_agnostic_indices = all_indices[~mask]
-    
-    language_agnostic_neurons[cur_module_name] = sorted_agnostic_indices.tolist()
-    language_specific_neurons[cur_module_name] = sorted_specific_indices.tolist()
-    
-    for indice in sorted_specific_indices:
-        max_value = 0
-        max_value_correspond_lang = ""
-        for i in range(len(lang_code_list)):
-            if sum_importance_matrix_list[i][indice] > max_value:
-                max_value = sum_importance_matrix_list[i][indice]
-                max_value_correspond_lang = lang_code_list[i]
-        language_specific_neurons_per_lang[cur_module_name][max_value_correspond_lang].append(indice.item())
+        # 排序后的语言特有神经元索引
+        sorted_specific_indices, _ = torch.sort(indices)
+        all_indices = torch.arange(all_sum_importance_matrix_var.size(0))
+        mask = torch.isin(all_indices, sorted_specific_indices)
+        # 排序后的语言共有神经元索引
+        sorted_agnostic_indices = all_indices[~mask]
+        
+        language_agnostic_neurons[cur_module_name] = sorted_agnostic_indices.tolist()
+        language_specific_neurons[cur_module_name] = sorted_specific_indices.tolist()
+        
+        # 将语言特有神经元细分到不同的语言中
+        for indice in sorted_specific_indices:
+            max_value = 0
+            max_value_correspond_lang = ""
+            for i in range(len(lang_code_list)):
+                if sum_importance_matrix_list[i][indice] > max_value:
+                    max_value = sum_importance_matrix_list[i][indice]
+                    max_value_correspond_lang = lang_code_list[i]
+            language_specific_neurons_per_lang[cur_module_name][max_value_correspond_lang].append(indice.item())
 
     # 将找到的语言共有神经元和语言特有神经元保存
     with open(folder_path + 'lang_agnos.json', 'w') as json_file:
---
+++ lang_cluster_visualize.py
@@ -0,0 +1,133 @@
+from transformers import AutoTokenizer, BloomForCausalLM
+import torch
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import copy
+from find_important_neuron import find_all_target_modules, read_monolingual_data
+import json
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+def get_lang_agnos_speci_neuron():
+    folder_path = "/data/lypan/llm_interpre/neuron_info/bloom-560m/"
+    with open(folder_path + 'lang_agnos.json', 'r') as json_file:
+        lang_agnos = json.load(json_file)
+    with open(folder_path + 'lang_speci.json', 'r') as json_file:
+        lang_speci = json.load(json_file)
+
+    return lang_agnos, lang_speci
+
+def forward_hook(module, input, output, module_name):
+    # 在前向传播时调用
+    forward_cache.append(output)
+
+def add_hooks(model, target_module_names):
+    hook_forwards = []
+    for name, module in model.named_modules():
+        if name in target_module_names:
+            print(name, module)
+            handle_forward = module.register_forward_hook(lambda m, i, o, module_name=name: forward_hook(m, i, o, name))
+            hook_forwards.append(handle_forward)
+
+    return hook_forwards
+
+
+if __name__ == "__main__":
+    lang_code_list = ['arb_Arab', 'fra_Latn', 'spa_Latn', 'eng_Latn', 'deu_Latn', 'ita_Latn', 'jpn_Jpan', 'rus_Cyrl', 'zho_Hans', 'zho_Hant']
+
+    model_path = "/data/lypan/llms/bloom-560m"
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    bloom = BloomForCausalLM.from_pretrained(model_path).to("cuda")
+
+    target_module_name_list = find_all_target_modules(bloom)
+    # 目标结构名称列表
+    target_module_names = ["transformer.h.12.mlp.dense_h_to_4h"]
+
+    language_agnostic_representation = {key: [] for key in target_module_names}
+    language_specific_representation = {key: [] for key in target_module_names}
+
+    forward_cache = []
+    hook_forwards = add_hooks(bloom, target_module_names)
+
+    repre_method = "part"
+
+    language_agnostic_neurons, language_specific_neurons = get_lang_agnos_speci_neuron()
+
+    for i in range(len(lang_code_list)):
+        cur_lang_code = lang_code_list[i]
+
+        sent_list = read_monolingual_data(cur_lang_code)
+
+        for j in tqdm(range(len(sent_list))):
+            text = sent_list[j]
+            inputs = tokenizer(text, return_tensors="pt").to("cuda")
+
+            input = inputs["input_ids"][:, 0:len(inputs["input_ids"][0]) - 1]
+            label = inputs["input_ids"][:, 1:len(inputs["input_ids"][0])]
+            outputs = bloom(input, labels=label)
+
+            for k in range(len(target_module_names)):
+                cur_module_name = target_module_names[k]
+
+                if repre_method == "all":
+                    agnostic_length = len(language_specific_neurons[cur_module_name])
+                    specific_length = len(language_agnostic_neurons[cur_module_name])
+                    all_indices = torch.arange(agnostic_length + specific_length)
+
+                    agnostic_bool_mask_matrix = torch.isin(all_indices, torch.tensor(language_specific_neurons[cur_module_name]))
+                    specific_bool_mask_matrix = ~agnostic_bool_mask_matrix
+
+                    agnostic_mask_matrix = torch.where(agnostic_bool_mask_matrix, torch.tensor(0), torch.tensor(1))
+                    specific_mask_matrix = torch.where(specific_bool_mask_matrix, torch.tensor(0), torch.tensor(1))
+                    # print(agnostic_mask_matrix)
+
+                    language_agnostic_representation[cur_module_name].append(forward_cache[k].detach().cpu() * agnostic_mask_matrix)
+                    language_specific_representation[cur_module_name].append(forward_cache[k].detach().cpu() * specific_mask_matrix)
+                    
+                elif repre_method == "part":
+                    language_agnostic_representation[cur_module_name].append(forward_cache[k].detach().cpu().index_select(
+                        -1, torch.tensor(language_agnostic_neurons[target_module_names[k]])))
+                    language_specific_representation[cur_module_name].append(forward_cache[k].detach().cpu().index_select(
+                        -1, torch.tensor(language_specific_neurons[target_module_names[k]])))
+
+            forward_cache = []
+
+    mean_agnostic_repre = [tensor.mean(dim=1) for tensor in language_agnostic_representation["transformer.h.12.mlp.dense_h_to_4h"]]
+    agnostic_repre = torch.stack(mean_agnostic_repre)
+    mean_agnostic_repre = agnostic_repre.view(-1, agnostic_repre.shape[-1]) # [lang_num * sent_num, lang_agnos_neuron_num]
+
+    mean_specific_repre = [tensor.mean(dim=1) for tensor in language_specific_representation["transformer.h.12.mlp.dense_h_to_4h"]]
+    specific_repre = torch.stack(mean_specific_repre)
+    mean_specific_repre = specific_repre.view(-1, specific_repre.shape[-1]) # [lang_num * sent_num, lang_speci_neuron_num]
+
+    # 语言共有神经元表示聚类
+    high_dimensional_data = mean_agnostic_repre
+
+    tsne = TSNE(n_components=2, perplexity=30, n_iter=300)
+    low_dimensional_data = tsne.fit_transform(high_dimensional_data)
+
+    colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#f1c40f', '#e67e22', '#e74c3c', '#5f27cd', '#95a5a6']
+    scatter_list = []
+    for i in range((int)(len(low_dimensional_data) / 997)):
+        start = i * 997
+        end = (i + 1) * 997
+        scatter_list.append(plt.scatter(low_dimensional_data[start:end, 0], low_dimensional_data[start:end, 1], c=colors[i], s=2))
+    plt.title('lang_agnos neuron t-SNE Visualization')
+    plt.legend(scatter_list, lang_code_list)
+    plt.savefig('/data/lypan/llm_interpre/fig/lang_agnos.png')
+
+    # 语言特有神经元表示聚类
+    high_dimensional_data = mean_specific_repre
+
+    tsne = TSNE(n_components=2, perplexity=30, n_iter=300)
+    low_dimensional_data = tsne.fit_transform(high_dimensional_data)
+
+    colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#f1c40f', '#e67e22', '#e74c3c', '#5f27cd', '#95a5a6']
+    scatter_list = []
+    for i in range((int)(len(low_dimensional_data) / 997)):
+        start = i * 997
+        end = (i + 1) * 997
+        scatter_list.append(plt.scatter(low_dimensional_data[start:end, 0], low_dimensional_data[start:end, 1], c=colors[i], s=2))
+    plt.title('lang_speci neuron t-SNE Visualization')
+    plt.legend(scatter_list, lang_code_list)
+    plt.savefig('fig/lang_speci.png')
